efc81872520bc088060b9b21dda13ad1970f5079
Lucas500260 <114434306+Lucas500260@users.noreply.github.com>
2022-11-15 17:49:31 -0300

    Create nota.js

---
+++ nota.js
@@ -0,0 +1,16 @@
+var Nota1 = parseInt(prompt("digite sua primeira nota"))
+var Nota2 = parseInt(prompt("digite sua segunda nota"))
+var Nota3 = parseInt(prompt("digite sua terceira nota"))
+
+var operação = (( Nota1 + Nota2 + Nota3) / 3 );
+if (operação >= 7) {
+  print("aprovado")}
+else {
+  print("reprovado")
+}
+
+var Nota1 = parseInt(prompt("digite sua primeira nota"))
+var Nota2 = parseInt(prompt("digite sua segunda nota"))
+
+var operação = (  21 - (Nota1 + Nota2 ));
+print(operação)
